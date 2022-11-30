ab751ff5b4b5bf5e07690f5f70f758d4c4f3f7b0
Danyllo Moreira Loureiro <danyllinho2007@hotmail.com>
2022-11-30 11:47:00 -0300

    Destaques produtos

--- index.php
+++ index.php
@@ -29,16 +29,19 @@
         <!-- Rodapé -->
         <footer class="painel-footer" style="background: none;">
             <?php include 'rodape.php'; ?>
-            <a name="contato>&nbsp;</a>
+            <a name="contato">&nbsp;</a>
         </footer>
     </main>
     </main>
     
 </body>
 
-    <!-- Link arquivos Bootstrap js --><script src=" https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Link arquivos Bootstrap js -->
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
                 <script src="js/bootstrap.min.js"></script>
-                <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>  <script src="./slick/slick.js" type="text/javascript" charset="utf-8"></script>  <script type="text/javascript">
+                <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>  
+                <script src="./slick/slick.js" type="text/javascript" charset="utf-8"></script>  
+                <script type="text/javascript">
                     $(document).on('ready', function() {
                         $(".regular").slick({
                             dots: true,
@@ -49,6 +52,4 @@
                     });
                 </script>
                 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-                <script src="js/bootstrap.min.js"></script>
-
 </html>
