27046d3350d5af864d92a8d57f389e5c2d9c069d
Danyllo Moreira Loureiro <danyllinho2007@hotmail.com>
2022-12-01 11:40:35 -0300

    Alteração

--- produtos_geral.php
+++ produtos_geral.php
@@ -2,3 +2,15 @@
 include 'conn/connect.php';
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produtos</title>
+</head>
+<body>
+    
+</body>
+</html>
