7c51736fc932f57def1dd09aa2b900cce04b1ff2
Danyllo Moreira Loureiro <danyllinho2007@hotmail.com>
2022-12-07 11:16:12 -0300

    Produtos lista OK

--- admin/produtos_lista.php
+++ admin/produtos_lista.php
@@ -33,12 +33,93 @@ $nrows = $lista->num_rows;
                         <span class="hidden-xs">ADICIONAR</span>
                     </a>
                 </th>
-                <th></th>
             </thead>
-            <tbody>
-
-            </tbody>
+            
+            <tbody><!-- inicio corpo da tabela -->
+                <?php do {?><!-- inicio estrutura repetição -->
+                    <tr>
+                        <td class="hidden"><?php echo $row['id_produto']; ?></td>
+                        <td>
+                            <span class="visible-xs"><?php echo $row['sigla_tipo']?></span>
+                            <span class="hidden-xs"><?php echo $row['rotulo_tipo']?></span>
+                        </td>
+                        <td>
+                             
+                             <?php 
+                                if ($row['destaque_produto']=='Sim') {
+                                    echo '<span class="glyphicon glyphicon-heart text-danger" aria-hidden="true"></span>';
+                                }else {
+                                    echo '<span class="glyphicon glyphicon-ok text-info" aria-hidden="true"></span>';
+                                }
+                            ?>
+                            <?php echo $row['descri_produto']?>
+                        </td>
+                        <td>
+                            <?php echo $row['resumo_produto'];?>
+                        </td>
+                        <td>
+                            <?php echo number_format ($row['valor_produto'],2,',','.');?>
+                        </td>
+                        <td>
+                            <img src="../images/<?php echo $row['imagem_produto']; ?>" alt="<?php echo $row['descri_produto']?>" width="100px">
+                        </td>
+                        <td>
+                            <a href="produtos_atualiza.php?id_produto=<?php echo $row['id_produto']; ?>" class="btn btn-warning btn-block btn-xs">
+                                <span class="hidden-xs">ALTERAR</span>
+                                <span class="glyphicon glyphicon-refresh"></span>
+                            </a>
+                            <br>
+                            <button 
+                            data-nome="<?php echo $row['descri_produto']?>" 
+                            data-id="<?php echo $row['id_produto']; ?>"
+                            class="delete btn btn-xs btn-block btn-danger"
+                            >
+                                <span class="hidden-xs">EXCLUIR</span>
+                                <span class="glyphicon glyphicon-trash"></span>
+                            </button>
+                        </td>
+                    </tr>
+                <?php } while($row = $lista->fetch_assoc());?><!-- final estrutura repetição -->
+            </tbody><!-- final corpo da tabela -->
         </table>
     </main>
+    <!-- inicio do modal para excluir -->
+    <div class="modal fade" id="modalEdit" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button class="close" data-dimiss="modal" type="button">
+                        &times;
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Deseja mesmo excluir o item?
+                    <h4><span class="nome text-danger"></span></h4>
+                </div>
+                <div class="modal-footer">
+                    <a href="#" type="button" class="btn btn-danger delete-yes">
+                        Confirmar
+                    </a>
+                    <button class="btn btn-sucess" data-dismiss="modal">
+                        Cancelar
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Fim do modal -->
 </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script type="text/javascript">
+    $('.delete').on('click',function(){
+        // console.log('Viu como funciona!!');
+        var nome = $(this).data('nome'); //busca o nome com a descrição (data-nome)
+        var id = $(this).data('id'); // busca o id (data-id)
+        // console.log(id + '-' + nome); // exibe no console
+        $('span.nome').text(nome); // insere o nome do item na confirmação
+        $('a.delete-yes').attr('href','produtos_excluir.php?id_produto='+id); // chama o arquivo php para excluir o produto
+        $('#modalEdit').modal('show')// chamar o modal
+    });
+</script>
 </html>
